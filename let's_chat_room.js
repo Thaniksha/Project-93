dfc6331c1d2ce24ba794b91551d47e6402f7497d
Thaniksha <73345437+Thaniksha@users.noreply.github.com>
2021-06-12 16:18:53 +0530

    Add files via upload

---
+++ let's_chat_room.js
@@ -0,0 +1,12 @@
+   // Your web app's Firebase configuration
+   var firebaseConfig = {
+    apiKey: "AIzaSyBkBQOrRC3OhRDSMsyA0C46y-DPNdxrmSc",
+    authDomain: "let-s-chat-app-76800.firebaseapp.com",
+    databaseURL: "https://let-s-chat-app-76800-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-app-76800",
+    storageBucket: "let-s-chat-app-76800.appspot.com",
+    messagingSenderId: "296655936574",
+    appId: "1:296655936574:web:4c46a60516cb0d3d07856f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
